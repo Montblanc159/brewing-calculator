76eddebcc6171a61ceb52286a8977f0da4149a8b
GuillaumeZ <guillaume.zaugg@gmail.com>
2025-07-02 22:49:40 +0200

    add hops handling, refactor file

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,8 @@
+mod math;
+
 use eframe::*;
 use egui::*;
+use math::*;
 use serde::*;
 
 const DEFAULT_SPACING: f32 = 8.0;
@@ -27,6 +30,20 @@ struct Ferment {
 struct Hop {
     name: String,
     alpha_acids: f32,
+    addition_time: u8,
+    utilization: f32,
+    weight: f32,
+    ibu: f32,
+    ratio: u8,
+}
+
+#[derive(Deserialize, Serialize, Default)]
+struct WhirlpoolHop {
+    name: String,
+    alpha_acids: f32,
+    weight: f32,
+    utilization: f32,
+    ibu: f32,
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -38,7 +55,8 @@ pub struct BrewingCalcApp {
     style: String,
     abv: f32,
     ebc: u8,
-    ibu: u8,
+    ibu: f32,
+    bugu: f32,
     original_gravity: f32,
     final_gravity: f32,
     efficiency: u8,
@@ -46,6 +64,7 @@ pub struct BrewingCalcApp {
     mash_water_ratio: f32,
     evaporation_rate: f32,
     hops: Vec<Hop>,
+    whirlpool_hops: Vec<WhirlpoolHop>,
     fermentecibles: Vec<Fermentecible>,
     ferments: Vec<Ferment>,
     mash_water_vol: f32,
@@ -61,7 +80,8 @@ impl Default for BrewingCalcApp {
             style: String::from(""),
             abv: 5.0,
             ebc: 5,
-            ibu: 10,
+            ibu: 10.0,
+            bugu: 0.0,
             original_gravity: 12.0,
             final_gravity: 2.0,
             efficiency: 80,
@@ -69,6 +89,7 @@ impl Default for BrewingCalcApp {
             evaporation_rate: 10.0,
             batch_size: 20,
             hops: vec![],
+            whirlpool_hops: vec![],
             fermentecibles: vec![],
             ferments: vec![],
             mash_water_vol: 0.0,
@@ -159,13 +180,30 @@ impl App for BrewingCalcApp {
 
             ui.add_space(DEFAULT_SPACING);
 
-            ui.label(format!("IBU : {}", self.ibu));
+            ui.horizontal(|ui| {
+                ui.label("IBU : ");
+                ui.add(Slider::new(&mut self.ibu, 0.0..=150.0));
+            });
+
+            ui.add_space(DEFAULT_SPACING);
+
+            ui.label(format!("BUGU : {}", self.bugu));
+
+            self.bugu = compute_bugu(self.ibu, self.original_gravity);
+
+            ui.add_space(DEFAULT_SPACING);
+
+            ui.label(format!(
+                "Required cell count : {}",
+                compute_cell_count(self.original_gravity, self.batch_size)
+            ));
 
             ui.add_space(DEFAULT_SPACING);
 
             ui.horizontal(|ui| {
                 ui.label("Densité initiale (°P) : ");
                 ui.add(Slider::new(&mut self.original_gravity, 0.0..=25.0));
+                ui.label(format!("{} G", convert_plato_to_sg(self.original_gravity)));
             });
 
             ui.add_space(DEFAULT_SPACING);
@@ -238,8 +276,8 @@ impl App for BrewingCalcApp {
 
             ui.add_space(DEFAULT_SPACING);
 
-            for index in 0..self.ferments.len() {
-                ferment_ui(ui, &mut self.ferments, index);
+            for ferment in &mut self.ferments {
+                ferment_ui(ui, ferment);
             }
 
             // This is an arbitrary choice to handle cofermentations, has to be enhanced
@@ -272,27 +310,29 @@ impl App for BrewingCalcApp {
 
             ui.add_space(DEFAULT_SPACING);
 
-            for index in 0..self.fermentecibles.len() {
-                fermentecible_ui(ui, &mut self.fermentecibles, index);
-
-                self.fermentecibles[index].weight = compute_grain_bill(
-                    compute_total_extract(self.original_gravity),
-                    self.efficiency,
-                    self.batch_size,
-                    &self.fermentecibles[index],
-                );
-
-                self.fermentecibles[index].mcu = compute_mcu(
-                    self.fermentecibles[index].ebc,
-                    self.fermentecibles[index].weight,
-                    self.batch_size,
-                );
-            }
-
             let mut weights = vec![];
             let mut mcus = vec![];
             let mut ratios = vec![];
-            for fermentecible in &self.fermentecibles {
+
+            for fermentecible in &mut self.fermentecibles {
+                let total_extract: f32 = compute_total_extract(self.original_gravity);
+
+                let fermentecible_extractable = compute_per_malt_extractable(
+                    total_extract,
+                    fermentecible.ratio,
+                    self.efficiency,
+                );
+
+                fermentecible.weight = compute_grain_bill(
+                    self.batch_size,
+                    fermentecible_extractable,
+                    fermentecible.humidity,
+                    fermentecible.extract,
+                );
+
+                fermentecible.mcu =
+                    compute_mcu(fermentecible.ebc, fermentecible.weight, self.batch_size);
+
                 ratios.push(fermentecible.ratio);
                 weights.push(fermentecible.weight);
                 mcus.push(fermentecible.mcu);
@@ -300,7 +340,11 @@ impl App for BrewingCalcApp {
 
             self.ebc = compute_ebc(mcus.iter().sum());
 
-            if self.fermentecibles.len() > 0 && check_fermentecible_ratio(ratios) {
+            for fermentecible in &mut self.fermentecibles {
+                fermentecible_ui(ui, fermentecible);
+            }
+
+            if self.fermentecibles.len() > 0 && check_ratios(ratios) {
                 ui.colored_label(
                     ERROR_COLOR,
                     "Problème de ratios : leur somme doit être égal à 100",
@@ -326,9 +370,45 @@ impl App for BrewingCalcApp {
             ui.add_space(DEFAULT_SPACING);
 
             ui.horizontal(|ui| {
+                ui.label("Houblons au whirlpool");
+                if ui.button("+").clicked() {
+                    self.whirlpool_hops.push(WhirlpoolHop {
+                        utilization: 0.12,
+                        weight: 1.0,
+                        ..Default::default()
+                    })
+                };
+
+                if ui.button("-").clicked() {
+                    self.whirlpool_hops.pop();
+                };
+            });
+
+            ui.add_space(DEFAULT_SPACING);
+
+            let mut ibu_left = self.ibu.clone();
+
+            for hop in &mut self.whirlpool_hops {
+                whirlpool_hop_ui(ui, hop);
+
+                hop.ibu = compute_ibu(
+                    hop.utilization,
+                    self.batch_size,
+                    hop.alpha_acids,
+                    hop.weight,
+                    self.original_gravity,
+                );
+
+                ibu_left -= hop.ibu;
+            }
+
+            ui.add_space(DEFAULT_SPACING);
+
+            ui.horizontal(|ui| {
                 ui.label("Houblons");
                 if ui.button("+").clicked() {
                     self.hops.push(Hop {
+                        alpha_acids: 5.0,
                         ..Default::default()
                     })
                 };
@@ -340,8 +420,37 @@ impl App for BrewingCalcApp {
 
             ui.add_space(DEFAULT_SPACING);
 
-            for index in 0..self.hops.len() {
-                hop_ui(ui, &mut self.hops, index);
+            let mut hop_ratios = vec![];
+            for hop in &mut self.hops {
+                hop_ui(ui, hop);
+
+                hop.utilization = compute_hop_utilization(self.original_gravity, hop.addition_time);
+
+                hop.weight = compute_hop_weight(
+                    hop.utilization,
+                    self.batch_size,
+                    hop.alpha_acids,
+                    ibu_left * (hop.ratio as f32 / 100.0),
+                    self.original_gravity,
+                );
+
+                hop.ibu = compute_ibu(
+                    hop.utilization,
+                    self.batch_size,
+                    hop.alpha_acids,
+                    hop.weight,
+                    self.original_gravity,
+                );
+
+                hop_ratios.push(hop.ratio)
+            }
+
+            if self.hops.len() > 0 && check_ratios(hop_ratios) {
+                ui.colored_label(
+                    ERROR_COLOR,
+                    "Problème de ratios : leur somme doit être égal à 100",
+                );
+                ui.add_space(DEFAULT_SPACING);
             }
 
             ui.separator();
@@ -367,10 +476,8 @@ fn powered_by_egui_and_eframe(ui: &mut Ui) {
     });
 }
 
-fn ferment_ui(ui: &mut Ui, ferments: &mut Vec<Ferment>, index: usize) {
+fn ferment_ui(ui: &mut Ui, ferment: &mut Ferment) {
     ui.horizontal(|ui| {
-        let ferment = &mut ferments[index];
-
         ui.text_edit_singleline(&mut ferment.name);
         ui.label("Atténuation (%)");
         ui.add(Slider::new(&mut ferment.attenuation, 0..=100));
@@ -380,10 +487,8 @@ fn ferment_ui(ui: &mut Ui, ferments: &mut Vec<Ferment>, index: usize) {
     ui.add_space(DEFAULT_SPACING);
 }
 
-fn fermentecible_ui(ui: &mut Ui, fermentecibles: &mut Vec<Fermentecible>, index: usize) {
+fn fermentecible_ui(ui: &mut Ui, fermentecible: &mut Fermentecible) {
     ui.horizontal(|ui| {
-        let fermentecible = &mut fermentecibles[index];
-
         ui.text_edit_singleline(&mut fermentecible.name);
         ui.label("Extrait (%)");
         ui.add(Slider::new(&mut fermentecible.extract, 0.0..=100.0));
@@ -398,91 +503,32 @@ fn fermentecible_ui(ui: &mut Ui, fermentecibles: &mut Vec<Fermentecible>, index:
     ui.add_space(DEFAULT_SPACING);
 }
 
-fn hop_ui(ui: &mut Ui, hops: &mut Vec<Hop>, index: usize) {
+fn hop_ui(ui: &mut Ui, hop: &mut Hop) {
     ui.horizontal(|ui| {
-        let hop = &mut hops[index];
-
         ui.text_edit_singleline(&mut hop.name);
         ui.label("Acide alpha (%)");
         ui.add(Slider::new(&mut hop.alpha_acids, 0.0..=100.0));
-        if ui.button("Supprimer").clicked() {
-            hops.remove(index);
-        }
+        ui.label(format!("Poids (g) : {}", hop.weight));
+        ui.label("Temps d'addition");
+        ui.add(Slider::new(&mut hop.addition_time, 0..=60));
+        ui.label("Ratio");
+        ui.add(Slider::new(&mut hop.ratio, 0..=100));
+        ui.label(format!("Utilisation : {}", hop.utilization));
+        ui.label(format!("Contribution IBU : {}", hop.ibu));
     });
     ui.add_space(DEFAULT_SPACING);
 }
 
-fn compute_abv(og: f32, fg: f32) -> f32 {
-    (og - fg) * 0.5
-}
-
-fn check_fermentecible_ratio(ratios: Vec<u8>) -> bool {
-    ratios.iter().sum::<u8>() != 100
-}
-
-// g/L
-fn compute_total_extract(og: f32) -> f32 {
-    (0.9974 / ((1.0 / og) - 0.00382) + 0.01) * 10.0
-}
-
-fn compute_grain_bill(
-    total_extract: f32,
-    efficiency: u8,
-    batch_size: u16,
-    fermentecible: &Fermentecible,
-) -> f32 {
-    let f_extract =
-        (total_extract * (fermentecible.ratio as f32 / 100.0)) / (efficiency as f32 / 100.0);
-
-    (f_extract / (fermentecible.extract / 100.0)) / (1.0 - (fermentecible.humidity / 100.0))
-        * batch_size as f32
-}
-
-fn compute_final_gravity(og: f32, attenuation: f32) -> f32 {
-    og - og * (attenuation / 100.0)
-}
-
-fn compute_mash_water_vol(grain_weight: f32, water_ratio: f32) -> f32 {
-    grain_weight / 1000.0 * water_ratio
-}
-
-fn compute_post_mash_water_vol(mash_water_vol: f32, grain_weight: f32) -> f32 {
-    mash_water_vol - ((grain_weight / 1000.0) * 0.8)
-}
-
-fn compute_sparge_water_vol(
-    batch_size: u16,
-    evaporation_rate: f32,
-    post_mash_water_vol: f32,
-) -> f32 {
-    (batch_size as f32 + (batch_size as f32 * (evaporation_rate as f32 / 100.0)))
-        - post_mash_water_vol
-}
-
-fn compute_pre_ebullition_water_vol(sparge_water_vol: f32, post_mash_water_vol: f32) -> f32 {
-    sparge_water_vol + post_mash_water_vol
-}
-
-fn compute_cell_count(og: f32, batch_size: u16) -> f32 {
-    1_000_000.0 * (batch_size as f32 * 1000.0) * og
-}
-
-fn compute_mcu(ebc: u8, grain_weight: f32, batch_size: u16) -> f32 {
-    (4.23 * (ebc as f32) * (grain_weight / 1000.0)) / batch_size as f32
-}
-
-fn compute_ebc(total_mcu: f32) -> u8 {
-    (2.939 * total_mcu.powf(0.6859)) as u8
-}
-
-fn compute_bugu(ibu: u8, og: f32) {
-    ibu as f32 / convert_plato_to_sg(og);
-}
-
-fn convert_plato_to_sg(plato: f32) -> f32 {
-    1.0 + (plato / (258.6 - ((plato / 258.2) * 227.1)))
-}
-
-fn convert_sg_to_plato(sg: f32) -> f32 {
-    (-1.0 * 616.868) + (1111.14 * sg) - (630.272 * sg.powf(2.0)) + (135.997 * sg.powf(3.0))
+fn whirlpool_hop_ui(ui: &mut Ui, hop: &mut WhirlpoolHop) {
+    ui.horizontal(|ui| {
+        ui.text_edit_singleline(&mut hop.name);
+        ui.label("Acide alpha (%)");
+        ui.add(Slider::new(&mut hop.alpha_acids, 0.0..=100.0));
+        ui.label("Weight (g)");
+        ui.add(Slider::new(&mut hop.weight, 0.0..=10000.0));
+        ui.label("Temps d'addition: Whirlpool");
+        ui.label(format!("Utilisation : {}", hop.utilization));
+        ui.label(format!("Contribution IBU : {}", hop.ibu));
+    });
+    ui.add_space(DEFAULT_SPACING);
 }
---
+++ src/app/math.rs
@@ -0,0 +1,127 @@
+pub fn compute_abv(og: f32, fg: f32) -> f32 {
+    (og - fg) * 0.5
+}
+
+pub fn check_ratios(ratios: Vec<u8>) -> bool {
+    ratios.iter().sum::<u8>() != 100
+}
+
+// g/L
+pub fn compute_total_extract(og: f32) -> f32 {
+    (0.9974 / ((1.0 / og) - 0.00382) + 0.01) * 10.0
+}
+
+pub fn compute_per_malt_extractable(total_extract: f32, malt_ratio: u8, efficiency: u8) -> f32 {
+    (total_extract * (malt_ratio as f32 / 100.0)) / (efficiency as f32 / 100.0)
+}
+
+pub fn compute_grain_bill(
+    batch_size: u16,
+    malt_extractable: f32,
+    malt_humidity: f32,
+    malt_extract: f32,
+) -> f32 {
+    // 81 /
+    (malt_extractable / (malt_extract / 100.0)) / (1.0 - (malt_humidity / 100.0))
+        * batch_size as f32
+}
+
+pub fn compute_final_gravity(og: f32, attenuation: f32) -> f32 {
+    og - og * (attenuation / 100.0)
+}
+
+pub fn compute_mash_water_vol(grain_weight: f32, water_ratio: f32) -> f32 {
+    grain_weight / 1000.0 * water_ratio
+}
+
+pub fn compute_post_mash_water_vol(mash_water_vol: f32, grain_weight: f32) -> f32 {
+    mash_water_vol - ((grain_weight / 1000.0) * 0.8)
+}
+
+pub fn compute_sparge_water_vol(
+    batch_size: u16,
+    evaporation_rate: f32,
+    post_mash_water_vol: f32,
+) -> f32 {
+    (batch_size as f32 + (batch_size as f32 * (evaporation_rate as f32 / 100.0)))
+        - post_mash_water_vol
+}
+
+pub fn compute_pre_ebullition_water_vol(sparge_water_vol: f32, post_mash_water_vol: f32) -> f32 {
+    sparge_water_vol + post_mash_water_vol
+}
+
+pub fn compute_mcu(ebc: u8, grain_weight: f32, batch_size: u16) -> f32 {
+    (4.23 * (ebc as f32) * (grain_weight / 1000.0)) / batch_size as f32
+}
+
+pub fn compute_ebc(total_mcu: f32) -> u8 {
+    (2.939 * total_mcu.powf(0.6859)) as u8
+}
+
+pub fn compute_bugu(ibu: f32, og: f32) -> f32 {
+    ibu / ((convert_plato_to_sg(og) - 1.0) * 1000.0)
+}
+
+///https://www.brewersfriend.com/plato-to-sg-conversion-chart/
+pub fn convert_plato_to_sg(plato: f32) -> f32 {
+    1.0 + (plato / (258.6 - ((plato / 258.2) * 227.1)))
+}
+
+// pub fn convert_sg_to_plato(sg: f32) -> f32 {
+//     (-1.0 * 616.868) + (1111.14 * sg) - (630.272 * sg.powf(2.0)) + (135.997 * sg.powf(3.0))
+// }
+
+pub fn compute_cell_count(og: f32, batch_size: u16) -> f32 {
+    1_000_000.0 * (batch_size as f32 * 1000.0) * og
+}
+
+/// Hop utilization is caculated using Glenn Tinseth's formula
+/// http://univers-biere.net/amertume.php
+/// https://www.backtoschoolbrewing.com/blog/2016/9/5/how-to-calculate-ibus
+///
+/// density argument is expressed in plato and converted in specific gravity
+pub fn compute_hop_utilization(density: f32, time: u8) -> f32 {
+    let density = convert_plato_to_sg(density);
+
+    let density_pow: f32 = density - 1.0;
+    let time_pow: f32 = -0.04 * (time as f32);
+
+    1.65 * 0.000125_f32.powf(density_pow) * ((1.0 - 2.71828_f32.powf(time_pow)) / 4.15) + 0.10
+}
+
+pub fn compute_ibu(
+    hop_utilization: f32,
+    batch_size: u16,
+    alpha: f32,
+    hop_weight: f32,
+    density: f32,
+) -> f32 {
+    let density_in_sg = convert_plato_to_sg(density);
+    if density_in_sg > 1.050 {
+        (hop_weight * hop_utilization * alpha as f32 * 10.0)
+            / (batch_size as f32 * density_correction(density_in_sg))
+    } else {
+        (hop_weight * hop_utilization * alpha as f32 * 10.0) / batch_size as f32
+    }
+}
+
+pub fn compute_hop_weight(
+    hop_utilization: f32,
+    batch_size: u16,
+    alpha: f32,
+    ibu: f32,
+    density: f32,
+) -> f32 {
+    let density_in_sg = convert_plato_to_sg(density);
+    if density_in_sg > 1.050 {
+        (batch_size as f32 * density_correction(density_in_sg) * ibu)
+            / (hop_utilization * alpha as f32 * 10.0)
+    } else {
+        (batch_size as f32 * ibu) / (hop_utilization * alpha as f32 * 10.0)
+    }
+}
+
+pub fn density_correction(density: f32) -> f32 {
+    1.0 + (((density / 1000.0) - 1.050) / 2.0)
+}
